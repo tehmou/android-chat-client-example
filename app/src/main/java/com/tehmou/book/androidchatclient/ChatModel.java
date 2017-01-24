2083c4b5fdb03c49c8b4f01bedee5fed69497ab6
tehmou <timo.tuominen@gmail.com>
2017-01-24 19:58:45 +0100

    Add ChatModel

---
+++ app/src/main/java/com/tehmou/book/androidchatclient/ChatModel.java
@@ -0,0 +1,81 @@
+package com.tehmou.book.androidchatclient;
+
+import android.util.Log;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import com.google.gson.Gson;
+
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposables;
+
+public class ChatModel {
+    private static final String TAG = ChatModel.class.getSimpleName();
+
+    private final Gson gson = new Gson();
+    private Socket socket;
+    private final ChatStore chatStore = new ChatStore();
+    private final CompositeDisposable subscriptions = new CompositeDisposable();
+
+    public void onCreate() {
+        // Create WebSocket
+        socket = createSocket();
+        socket.on("connect", args ->
+                Log.d(TAG, "Socket connected"));
+        subscriptions.add(
+                createListener(socket)
+                        .map(json -> gson.fromJson(json, ChatMessage.class))
+                        .map(chatMessage -> chatMessage.setIsPending(false))
+                        .subscribe(chatStore::put)
+        );
+    }
+
+    public void onDestroy() {
+        subscriptions.clear();
+    }
+
+    public void connect() {
+        // Connect WebSocket
+        socket.connect();
+    }
+
+    public void disconnect() {
+        // Disconnect WebSocket
+        socket.disconnect();
+    }
+
+    public void sendMessage(String message) {
+        ChatMessage chatMessage = new ChatMessage(message);
+        chatStore.put(chatMessage);
+        socket.emit("chat message", gson.toJson(chatMessage));
+    }
+
+    public Observable<Collection<ChatMessage>> getChatMessages() {
+        return chatStore.getStream();
+    }
+
+    public static Observable<String> createListener(Socket socket) {
+        return Observable.create(subscriber -> {
+            Emitter.Listener listener = args -> subscriber.onNext((String) args[0]);
+            socket.on("chat message", listener);
+            subscriber.setDisposable(Disposables.fromAction(
+                    () -> socket.off("chat message", listener)
+            ));
+        });
+    }
+
+    public static Socket createSocket() {
+        Socket socket = null;
+        try {
+            socket = IO.socket(Config.SERVER_URL);
+        } catch (URISyntaxException e) {
+            Log.e(TAG, "Error creating socket", e);
+        }
+        return socket;
+    }
+}
--- app/src/main/java/com/tehmou/book/androidchatclient/MainActivity.java
+++ app/src/main/java/com/tehmou/book/androidchatclient/MainActivity.java
@@ -2,28 +2,17 @@ package com.tehmou.book.androidchatclient;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import com.github.nkzawa.emitter.Emitter;
-import com.github.nkzawa.socketio.client.IO;
-import com.github.nkzawa.socketio.client.Socket;
-import com.google.gson.Gson;
-
-import java.net.URISyntaxException;
-
-import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposables;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private Socket socket;
-    private ChatStore chatStore;
+    private ChatModel chatModel;
     private ChatViewModel chatViewModel;
     private final CompositeDisposable viewSubscriptions = new CompositeDisposable();
 
@@ -32,23 +21,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create and connect WebSocket
-        socket = createSocket();
-        socket.on("connect", args ->
-                Log.d(TAG, "Socket connected"));
-        socket.connect();
+        chatModel = new ChatModel();
+        chatModel.onCreate();
+        chatModel.connect();
 
-        Gson gson = new Gson();
-
-        chatStore = new ChatStore();
-        viewSubscriptions.add(
-            createListener(socket)
-                    .map(json -> gson.fromJson(json, ChatMessage.class))
-                    .map(chatMessage -> chatMessage.setIsPending(false))
-                    .subscribe(chatStore::put)
-        );
-
-        chatViewModel = new ChatViewModel(chatStore.getStream());
+        chatViewModel = new ChatViewModel(chatModel.getChatMessages());
+        chatViewModel.subscribe();
 
         ListView listView = (ListView) findViewById(R.id.list_view);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
@@ -64,12 +42,8 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.edit_text);
         findViewById(R.id.send_button)
                 .setOnClickListener(event -> {
-                    ChatMessage chatMessage = new ChatMessage(editText.getText().toString());
-                    chatStore.put(chatMessage);
-                    socket.emit("chat message", gson.toJson(chatMessage));
+                    chatModel.sendMessage(editText.getText().toString());
                 });
-
-        chatViewModel.subscribe();
     }
 
     @Override
@@ -78,27 +52,7 @@ public class MainActivity extends AppCompatActivity {
         chatViewModel.unsubscribe();
         viewSubscriptions.clear();
 
-        // Disconnect WebSocket
-        socket.disconnect();
-    }
-
-    public static Observable<String> createListener(Socket socket) {
-        return Observable.create(subscriber -> {
-            Emitter.Listener listener = args -> subscriber.onNext((String) args[0]);
-            socket.on("chat message", listener);
-            subscriber.setDisposable(Disposables.fromAction(
-                    () -> socket.off("chat message", listener)
-            ));
-        });
-    }
-
-    public static Socket createSocket() {
-        Socket socket = null;
-        try {
-            socket = IO.socket(Config.SERVER_URL);
-        } catch (URISyntaxException e) {
-            Log.e(TAG, "Error creating socket", e);
-        }
-        return socket;
+        chatModel.disconnect();
+        chatModel.onDestroy();
     }
 }
